85f02b45f817aab1d81d18282276b1efe608b9ca
CrystalW11 <crystal.warmack@gmail.com>
2025-04-06 20:31:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+// ----------------------------------------------------------------
+
+
+/*This code is merely "one possible solution." You could have eliminated the temporary variable flip by calling the Next() inside of the Boolean expression like so:*/
+
+Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+
